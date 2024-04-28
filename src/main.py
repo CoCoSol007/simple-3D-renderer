6b08562c37745bf166b3854c2487d0ad2ae06f70
CoCoSol <solois.corentin@gmail.com>
2024-04-28 10:54:47 +0200

    Change default exemple

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@
 from renderer.renderer import Renderer, ViewCamera
 
 # create the renderer with the size of 400x400 with the view calculed by PYTHAGORE, you can also chose THALES
-renderer = Renderer(400, 400, ViewCamera.PYTHAGORE)
+renderer = Renderer(400, 400, ViewCamera.THALES)
 
 renderer.new_square(0,0,-4, 1, 1, 1)
 renderer.new_triangle(1,0,-4, 1, 1, 1)
